f14119fea048f8ae95ec21fb31dae3cbdbc3769e
bg-write <bradywgerber@gmail.com>
2022-12-16 16:11:45 -0600

    set js outline of topics

--- javascript.js
+++ javascript.js
@@ -9,15 +9,21 @@ Created by Netscape in 1995 as an extension of HTML for Netscape Navigator 2.0 t
 /* this is a comment,
 with multiple lines */
 
-// to add JS to your HTML
-{/* <script src="script.js"></script>; */}
-
-// to call the script in the terminal
-// >
+// to add JS to your HTML (see our "index.html" file)
+{
+	/* <script src="script.js"></script>; */
+}
 
 console.log('Hello, World');
 
+// to call the script in the terminal
+// first, make sure you've installed Node.js
+// > node javascript.js
+
 //===================== VARIABLES, DATA AND VALUES ============
+var varVar = "I'm the original way to declare a variable, avoid me if you can";
+const varConst = 'I can NOT be reassigned';
+let varLet = 'I can be reassigned';
 
 //===================== DOING MATH ============================
 
@@ -30,8 +36,17 @@ console.log('Hello, World');
 //===================== ARRAYS ================================
 
 //===================== FUNCTIONS =============================
+// first-class functions
+
+//===================== OOP IN JS =============================
+
+//===================== DOM MANIPULATION ======================
+
+//===================== EVENT LISTENERS =======================
 
 //===================== IMPORTING JSON FILES ==================
+
+//===================== ASYNC/AWAIT & PROMISES ================
 
 //===================== JS SOLUTIONS TO KNOW ==================
 // todo Palindrome
@@ -40,9 +55,15 @@ console.log('Hello, World');
 
 // todo Fibonacci
 
-//===================== Thank you for reading :)
+//===================== NODE.JS ===============================
+// How we're able to build server-side apps with JS
+
+//==== JS FRAMEWORKS & LIBRARIES TO KNOW (AT LEAST ABOUT) =====
+
+//===================== Thank you for reading :) ==============
 /* 
-Helpful JS links
+Helpful JS links:
+JavaScript in 100 Seconds (Fireship) https://www.youtube.com/watch?v=DHjqpvDnNGE
 
 Thank you, General Assembly
 */
